3f031458aa1bd7d04fac3a25e994c01eeac52e2b
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-16 13:48:34 -0500

    Todo made

---
+++ src/Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 11
+Last Modified_______: 2019-03-16
+OU ID_______________: 113334522
+OU EMAIL____________: eid.s.zaben-1@ou.edu
+
+Tasks:
+1. Complete the gamePieceAppearance class
+2. Complete the BoardGame class
+3. Complete the enum classes
+4. Write the tests in the driver class
+5. Pass the tests that are written
+
+Estimated Time (minutes):
+1. 10
+2. 30
+3. 40
+4. 75
+5. 30
+
+Actual Time (minutes):
+1. 7
+2. 
+3. 
+4. 
+5. 
+*/}
