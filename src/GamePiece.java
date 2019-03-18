b321915028c12dbe0376a4fc6db331ba0d5e019c
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-18 08:36:39 -0500

    Formatted and added comments to Locaiton and GamePiece

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -1,43 +1,56 @@
 public enum GamePiece
 {
+    // Setting each gamePiece with parameters
     RED_RACER(new GamePieceAppearance(Color.RED, Shape.RACECAR), 0),
-     BLUE_RACER(new GamePieceAppearance(Color.BLUE, Shape.RACECAR), 2),
-     MAGENTA_RACER(new GamePieceAppearance(Color.MAGENTA, Shape.RACECAR), 1),
-     RED_THIMBLE(new GamePieceAppearance(Color.RED, Shape.THIMBLE), 10),
-   BLUE_BOOT(new GamePieceAppearance(Color.BLUE, Shape.BOOT), 5),
-   GREEN_BOOT(new GamePieceAppearance(Color.GREEN, Shape.BOOT), 8),
-   YELLOW_BOOT(new GamePieceAppearance(Color.YELLOW, Shape.BOOT), 7);
-    
+    BLUE_RACER(new GamePieceAppearance(Color.BLUE, Shape.RACECAR), 2),
+    MAGENTA_RACER(new GamePieceAppearance(Color.MAGENTA, Shape.RACECAR), 1),
+    RED_THIMBLE(new GamePieceAppearance(Color.RED, Shape.THIMBLE), 10),
+    BLUE_BOOT(new GamePieceAppearance(Color.BLUE, Shape.BOOT), 5),
+    GREEN_BOOT(new GamePieceAppearance(Color.GREEN, Shape.BOOT), 8),
+    YELLOW_BOOT(new GamePieceAppearance(Color.YELLOW, Shape.BOOT), 7);
+
+    // Instance variables
     private GamePieceAppearance appearance;
     private int priority;
-    
-    private GamePiece(GamePieceAppearance appearance, int priority) {
+
+    // Constructor with parameters
+    private GamePiece(GamePieceAppearance appearance, int priority)
+    {
         this.appearance = appearance;
         this.priority = priority;
 
     }
-    
-    public Color getColor() {
-     return appearance.getColor();   
+
+    // Getter for color
+    public Color getColor()
+    {
+        return appearance.getColor();
     }
-    
-    public Shape getShape() {
+
+    // Getter for shape
+    public Shape getShape()
+    {
         return appearance.getShape();
     }
-    
-    public GamePiece movesFirst(GamePiece a, GamePiece b) {
-        if (a.priority > b.priority) {
+
+    // Determines which piece moves first depending on each pieces priority
+    public GamePiece movesFirst(GamePiece a, GamePiece b)
+    {
+        if (a.priority > b.priority)
+        {
             return b;
-        } else {
+        } else
+        {
             return a;
         }
     }
-    
-    public String toString() {
-        return String.format("%s: a %s %s with priority %d", this.name(), this.getColor(), this.getShape(), this.priority);
-    }
-    
-}
 
-    
-    
+    // Overrides the previous toString
+    public String toString()
+    {
+        // Formatted using Javadoc
+        return String.format("%s: a %s %s with priority %d", this.name(), this.getColor(), this.getShape(),
+                this.priority);
+    }
+
+}
--- src/Location.java
+++ src/Location.java
@@ -1,4 +1,5 @@
 public enum Location
 {
+    //Setting the names of each location enum
     KITCHEN, CONSERVATORY, DINING_ROOM, BALLROOM, STUDY, HALL, LOUNGE, LIBRARY, BILLIARD_ROOM;
 }
