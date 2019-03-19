6b1153a63bde114119d10b36b3e43cc6bcb0c0f6
Eid Zaben <eid.s.zaben-1@ou.edu>
2019-03-19 17:52:30 -0500

    tests updated gamepiece toString

--- src/Driver.java
+++ src/Driver.java
@@ -14,9 +14,9 @@ public class Driver
      */
     public static void main(String[] args)
     {
-        // TODO Auto-generated method stub
-
-    }
+        GamePiece a = GamePiece.BLUE_BOOT;
+        System.out.println(a.toString());
+        }
 
     @Test
     public void testColorValues()
@@ -91,7 +91,58 @@ public class Driver
         GamePieceAppearance ex = new GamePieceAppearance(Color.RED, Shape.THIMBLE);
         Assert.assertEquals("GPA getColor incorrect", ex.getColor(), Color.RED);
         Assert.assertEquals("GPA getShape incorrect", ex.getShape(), Shape.THIMBLE);
+    }
+    
+    @Test
+    public void testGamePiece() {
+        GamePiece gp = null;
+        gp = GamePiece.BLUE_BOOT;
+        Assert.assertEquals("GamePiece color incorrect" + gp.name(), Color.BLUE, gp.getColor());
+        Assert.assertEquals("GamePiece shape incorrect" + gp.name(), Shape.BOOT, gp.getShape());
+        //Expected toString might be incorrect
+        Assert.assertEquals("GamePiece toString incorrect" + gp.name(), "BLUE_BOOT: a BLUE boot with priority 5", gp.toString());
+       
+        gp = GamePiece.RED_RACER;
+        Assert.assertEquals("GamePiece color incorrect" + gp.name(), Color.RED, gp.getColor());
+        Assert.assertEquals("GamePiece shape incorrect" + gp.name(), Shape.RACECAR, gp.getShape());
+        //Expected toString might be incorrect
+        Assert.assertEquals("GamePiece toString incorrect" + gp.name(), "RED_RACER: a RED racecar with priority 0", gp.toString());
+     
+        gp = GamePiece.BLUE_RACER;
+        Assert.assertEquals("GamePiece color incorrect" + gp.name(), Color.BLUE, gp.getColor());
+        Assert.assertEquals("GamePiece shape incorrect" + gp.name(), Shape.RACECAR, gp.getShape());
+        //Expected toString might be incorrect
+        Assert.assertEquals("GamePiece toString incorrect" + gp.name(), "BLUE_RACER: a BLUE racecar with priority 2", gp.toString());
         
+        gp = GamePiece.MAGENTA_RACER;
+        Assert.assertEquals("GamePiece color incorrect" + gp.name(), Color.MAGENTA, gp.getColor());
+        Assert.assertEquals("GamePiece shape incorrect" + gp.name(), Shape.RACECAR, gp.getShape());
+        //Expected toString might be incorrect
+        Assert.assertEquals("GamePiece toString incorrect" + gp.name(), "MAGENTA_RACER: a MAGENTA racecar with priority 1", gp.toString());
+        
+        gp = GamePiece.RED_THIMBLE;
+        Assert.assertEquals("GamePiece color incorrect" + gp.name(), Color.RED, gp.getColor());
+        Assert.assertEquals("GamePiece shape incorrect" + gp.name(), Shape.THIMBLE, gp.getShape());
+        //Expected toString might be incorrect
+        Assert.assertEquals("GamePiece toString incorrect" + gp.name(), "RED_THIMBLE: a RED thimble with priority 10", gp.toString());
+        
+        gp = GamePiece.GREEN_BOOT;
+        Assert.assertEquals("GamePiece color incorrect" + gp.name(), Color.GREEN, gp.getColor());
+        Assert.assertEquals("GamePiece shape incorrect" + gp.name(), Shape.BOOT, gp.getShape());
+        //Expected toString might be incorrect
+        Assert.assertEquals("GamePiece toString incorrect" + gp.name(), "GREEN_BOOT: a GREEN boot with priority 8", gp.toString());
+       
+        gp = GamePiece.YELLOW_BOOT;
+        Assert.assertEquals("GamePiece color incorrect" + gp.name(), Color.YELLOW, gp.getColor());
+        Assert.assertEquals("GamePiece shape incorrect" + gp.name(), Shape.BOOT, gp.getShape());
+        //Expected toString might be incorrect
+        Assert.assertEquals("GamePiece toString incorrect" + gp.name(), "YELLOW_BOOT: a YELLOW boot with priority 7", gp.toString());
+       
+        //Testing movesFirst method
+        GamePiece prior10 = GamePiece.RED_THIMBLE;
+        GamePiece prior2 = GamePiece.BLUE_RACER;
+        
+        Assert.assertEquals("Wrong game piece moving first", prior2, gp.movesFirst(prior10, prior2));
         
     }
 
